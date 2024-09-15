dacc591de511354c6efb9e7acc88aa0338c0972e
siroj05 <rojudin123@gmail.com>
2024-09-15 13:48:26 +0700

    add pokedex

--- app/list-pokemon/pokedex/page.tsx
+++ app/list-pokemon/pokedex/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+import PokemonCard from "@/components/card";
+import { results } from "@/lib/types";
+import { useEffect, useState } from "react";
+
 export default function Pokedex () {
+  const [pokedex, setPokedex] = useState<results[]>()
+  useEffect(() => {
+    const savedPokedex = localStorage.getItem("pokedex");
+    if (savedPokedex) {
+      setPokedex(JSON.parse(savedPokedex));
+    }
+  }, [])
+
   return (
-    <div>
-      
+    <div className="flex justify-center my-5">
+      <div className="grid grid-cols-4 gap-5">
+        {
+          pokedex?.map((item,i) => (
+            <PokemonCard
+              item={item}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
--- components/card.tsx
+++ components/card.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useStatePokedex } from "@/app/list-pokemon/pokedex/store";
+import { usePathname } from "next/navigation";
 
 type Props = {
   item: results;
@@ -24,50 +25,85 @@ const colorMap: { [key: string]: string } = {
 };
 
 export default function PokemonCard({ item, color }: Props) {
-  const { setPokedex } = useStatePokedex((state) => state)
+  const { setPokedex } = useStatePokedex((state) => state);
+  const path = usePathname();
+  
   useEffect(() => {
     const savedPokedex = localStorage.getItem("pokedex");
     if (savedPokedex) {
       setPokedex(JSON.parse(savedPokedex));
     }
-  }, [])
+  }, []);
 
   const handleClick = (newPokemon: results) => {
-    const savedPokedex = localStorage.getItem("pokedex")
-    let currentPokedex: results[] = savedPokedex ? JSON.parse(savedPokedex) : [];
-    const isAlreadyInPokedex = currentPokedex.some(pokemon => pokemon.name === newPokemon.name)
+    const savedPokedex = localStorage.getItem("pokedex");
+    let currentPokedex: results[] = savedPokedex
+      ? JSON.parse(savedPokedex)
+      : [];
+    const isAlreadyInPokedex = currentPokedex.some(
+      (pokemon) => pokemon.name === newPokemon.name
+    );
     if (!isAlreadyInPokedex) {
       const updatedPokedex = [...currentPokedex, newPokemon];
-      setPokedex(updatedPokedex)
-      localStorage.setItem("pokedex", JSON.stringify(updatedPokedex))
+      setPokedex(updatedPokedex);
+      localStorage.setItem("pokedex", JSON.stringify(updatedPokedex));
     } else {
-      console.log("Pokémon is already in the Pokedex.")
+      console.log("Pokémon is already in the Pokedex.");
     }
   };
 
-  let colorClass = colorMap[item.colors] || (color && colorMap[color]) || "bg-gray-200";
+  let colorClass =
+    colorMap[item.colors] || (color && colorMap[color]) || "bg-gray-200";
+
+  const handleDrop = (item:any) => {
+    const savedPokedex = localStorage.getItem("pokedex");
+    let currentPokedex: results[] = savedPokedex
+      ? JSON.parse(savedPokedex)
+      : [];
+    const updatedPokedex = currentPokedex.filter(
+      (pokemon) => pokemon.name !== item.name
+    );
+    setPokedex(updatedPokedex);
+    localStorage.setItem("pokedex", JSON.stringify(updatedPokedex));
+  }
 
   return (
     <div className="relative transform transition-transform duration-300 hover:scale-105">
-      <div className={`w-[200px] shadow-xl p-3 ${colorClass} rounded-md`}>
+      <div className={`w-[200px] md:w-[150px] shadow-xl p-3 ${colorClass} rounded-md`}>
         <Link href={`/list-pokemon/${item.name}`}>
           <img src={item.image} alt={item.name} className="w-full h-30" />
           <div className="font-bold text-center">{item.name}</div>
         </Link>
         <div className="flex justify-center">
-          <button
-            onClick={() => handleClick(item)}
-            className="hover:bg-blue-300 flex gap-2 bg-progress text-white rounded-sm px-5 border-2 border-white"
-          >
-            <span>Add</span>
-            <Image
-              className="hover:animate-spin my-auto"
-              src="/loading.png"
-              alt=""
-              width={14}
-              height={14}
-            />
-          </button>
+          {path === "/list-pokemon/pokedex" ? (
+            <button
+              onClick={() => handleDrop(item)}
+              className="hover:bg-blue-300 flex gap-2 bg-progress text-white rounded-sm px-5 border-2 border-white"
+            >
+              <span>Drop</span>
+              <Image
+                className="hover:animate-spin my-auto"
+                src="/loading.png"
+                alt=""
+                width={14}
+                height={14}
+              />
+            </button>
+          ) : (
+            <button
+              onClick={() => handleClick(item)}
+              className="hover:bg-blue-300 flex gap-2 bg-progress text-white rounded-sm px-5 border-2 border-white"
+            >
+              <span>Add</span>
+              <Image
+                className="hover:animate-spin my-auto"
+                src="/loading.png"
+                alt=""
+                width={14}
+                height={14}
+              />
+            </button>
+          )}
         </div>
       </div>
     </div>
