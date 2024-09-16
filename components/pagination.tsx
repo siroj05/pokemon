5b4dee7e9fd899dedfa3ee80add3010bffcc9c32
siroj05 <rojudin123@gmail.com>
2024-09-16 13:28:05 +0700

    fix responsive pagination

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -56,7 +56,7 @@ export default function PaginationPokemon(
           {currentPage - 1 !== 0 && (
             <Link
               href={createPageURL(currentPage - 1)}
-              className="px-4 py-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 transition"
+              className="px-4 py-2 max-sm:px-2 max-sm:py-1 text-sm max-sm:text-xs font-semibold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 transition"
             >
               Prev
             </Link>
@@ -65,13 +65,13 @@ export default function PaginationPokemon(
         {pageNumbers.map((number, index) => (
           <PaginationItem key={index}>
             {number === "..." ? (
-              <PaginationEllipsis className="px-2 py-1" />
+              <PaginationEllipsis className="px-2 py-1 max-sm:px-1 max-sm:py-0.5" />
             ) : (
               <Link
                 href={createPageURL(number)}
-                className={`px-4 py-2 text-sm font-semibold rounded-md transition ${
+                className={`px-4 py-2 max-sm:px-2 max-sm:py-1 text-sm max-sm:text-xs font-semibold rounded-md transition ${
                   number === currentPage
-                    ? "bg-header text-white"
+                    ? "bg-blue-500 text-white"
                     : "bg-gray-200 text-gray-700 hover:bg-gray-300"
                 }`}
               >
@@ -84,7 +84,7 @@ export default function PaginationPokemon(
           {currentPage < sum && (
             <Link
               href={createPageURL(currentPage + 1)}
-              className="px-4 py-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 transition"
+              className="px-4 py-2 max-sm:px-2 max-sm:py-1 text-sm max-sm:text-xs font-semibold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 transition"
             >
               Next
             </Link>
